3fa7fc8fba7e553e95ec2e6f0ecac6cd27efe054
Connie-Wild <34596022+Connie-Wild@users.noreply.github.com>
2022-01-12 19:24:32 +0900

    apt

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo apt clean -y
 # ------------------------
 # パッケージインストール
 # ------------------------
-sudo apt install -y build-essential libffi-dev zlib1g-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
+sudo apt install -y wget curl llvm build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # ------------------------
 # 時刻同期
