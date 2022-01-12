72dcd0d8e3adcf6173654774236afa2134e53837
Connie-Wild <34596022+Connie-Wild@users.noreply.github.com>
2022-01-12 19:58:15 +0900

    apt clean

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ sudo apt clean -y
 # パッケージインストール
 # ------------------------
 sudo apt install -y wget curl llvm build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt clean -y
 
 # ------------------------
 # 時刻同期
